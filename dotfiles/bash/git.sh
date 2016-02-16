9c69dffa32b56d61441ae3dd347e0e982ea3bb02
Ricardo <rgr@qosmotec.com>
2016-02-16 18:33:30 +0100

    removed git file peing parsed in windows

--- dotfiles/bash/git.sh
+++ dotfiles/bash/git.sh
@@ -1,3 +1,4 @@
+if $_islinux; then
 if $_isxrunning; then
     get_prompt_symbol() {
       [[ $UID == 0 ]] && echo "#" || echo "\$"
@@ -5,4 +6,5 @@ if $_isxrunning; then
 
    export GIT_PS1_SHOWCOLORHINTS=true
    export PS1="$GY[$Y\u$GY@$P\h$GY:$B\W\$(__git_ps1 \"$GY|$LB%s\")$GY]$W\$(get_prompt_symbol) "
+fi
 fi
