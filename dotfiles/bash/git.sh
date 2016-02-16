311659646a4a35cafae36dc40a8b0cce59411e69
Ricardo <rgr@qosmotec.com>
2016-02-16 18:50:48 +0100

    coloring etc :)

--- dotfiles/bash/git.sh
+++ dotfiles/bash/git.sh
@@ -1,10 +1,10 @@
 if $_islinux; then
-if $_isxrunning; then
-    get_prompt_symbol() {
-      [[ $UID == 0 ]] && echo "#" || echo "\$"
-    }
+	if $_isxrunning; then
+	    get_prompt_symbol() {
+	      [[ $UID == 0 ]] && echo "#" || echo "\$"
+	    }
 
-   export GIT_PS1_SHOWCOLORHINTS=true
-   export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1) \$ '
-fi
+	   export GIT_PS1_SHOWCOLORHINTS=true
+	   export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1) \$ '
+	fi
 fi
